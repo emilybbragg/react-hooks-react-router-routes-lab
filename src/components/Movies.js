2228ce64c2793fab8bdcd7da6cea0d6438d29cf3
Emily <emilybribragg@gmail.com>
2022-04-04 13:49:06 -0400

    commit tree a458b1a8d427e14c77d09ec36eb62b9119cb5faf

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { movies } from "../data";
 
+/*
+This component should render the text Movies Page in an <h1> and make a new <div> for each movie
+The <div> should contain the movie's title, time and a <ul> with a list of its genres, each within their own <li>.
+*/
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  const movieItems = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+    </div>
+  );
+};
 
 export default Movies;
